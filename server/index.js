d145737c4a626f85c444bfbc1f1f375aed860d45
Bruno Casotto <casottoalves@hotmail.com>
2019-11-07 09:52:52 -0300

    Created vscode tasks

--- server/index.js
+++ server/index.js
@@ -1,1 +1,28 @@
-console.log('casotto')
+const { ApolloServer } = require('apollo-server')
+
+const typeDefs = `
+  type Item {
+    id: Int
+    type: String
+    description: String
+  }
+
+  type Query {
+    prefixes: [Item],
+    sufixes: [Item]
+  }
+`
+
+const resolvers = {
+  Query: {
+    prefixes() {
+      return []
+    },
+    sufixes() {
+      return []
+    }
+  }
+}
+
+const server = new ApolloServer({ typeDefs, resolvers })
+server.listen()
