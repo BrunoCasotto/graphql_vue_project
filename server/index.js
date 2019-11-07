ca5be72f447d5424cd2b8b001dec0c6846b1acb9
Bruno Casotto <casottoalves@hotmail.com>
2019-11-07 17:42:43 -0300

    Initial apollo configuration

--- server/index.js
+++ server/index.js
@@ -8,18 +8,50 @@ const typeDefs = `
   }
 
   type Query {
-    prefixes: [Item],
-    sufixes: [Item]
+    items (type: String): [Item]
+  }
+
+  input ItemInput {
+    type: String
+    description: String
+  }
+
+  type Mutation {
+    saveItem (item: ItemInput): Item
+    deleteItem (id: Int): Boolean
   }
 `
 
+const prefixes = [
+  {id: 1, type: 'prefixe', description: 'Air'},
+  {id: 2, type: 'prefixe', description: 'Jet'},
+  {id: 3, type: 'prefixe', description: 'Plain'},
+  {id: 4, type: 'sufix', description: 'Hub'},
+  {id: 5, type: 'sufix', description: 'Mart'},
+  {id: 6, type: 'sufix', description: 'Test'}
+]
+
 const resolvers = {
   Query: {
-    prefixes() {
-      return []
+    items(_, args) {
+      const response = prefixes.filter(item => item.type === args.type)
+      return response
+    }
+  },
+  Mutation: {
+    saveItem(_, args) {
+      const { item } = args
+      item.id = math.floor(math.random() * 1000)
+      prefixes.push(item)
+      return item
     },
-    sufixes() {
-      return []
+    deleteItem(_, args) {
+      const { id } = args
+      const item = items.find(item => item.id === id)
+      if(!item) return false
+
+      items.splice(items.indexOf(item), 1)
+      return true
     }
   }
 }
