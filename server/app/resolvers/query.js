3fe4d95110006d1e73f9bc6d1cb47ad26d981694
Bruno Casotto <casottoalves@hotmail.com>
2019-11-07 18:17:11 -0300

    Update team query filter players added

--- server/app/resolvers/query.js
+++ server/app/resolvers/query.js
@@ -1,17 +1,34 @@
 const teamsData = require('./../data/teams.json')
+const playersData = require('./../data/players.json')
 
 const matchName = (nameWord, name) => {
   const regex = new RegExp(nameWord.toLowerCase(),'g')
   return name.toLowerCase().search(regex) != -1
 }
 
+const getPlayersByTeam = (teamId) => {
+  const players = playersData.players.filter(
+    player => player.teamId === teamId
+  )
+
+  return players
+}
+
 const teams = (_, args) => {
   const { name } = args
   if(!name) return teamsData.teams
 
-  const result = teamsData.teams.filter(
+  const teamsMatched = teamsData.teams.filter(
     team => matchName(name, team.teamName)
   )
+
+  const result = teamsMatched.map(team => {
+    return {
+      ...team,
+      players: getPlayersByTeam(team.teamId)
+    }
+  })
+
   return result
 }
 
