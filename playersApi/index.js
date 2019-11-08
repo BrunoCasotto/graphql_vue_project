7581b1af76e51f4cbb0bc715f4b27ae785d1e2e8
Bruno Casotto <casottoalves@hotmail.com>
2019-11-07 23:55:24 -0300

    apis mock query done

--- playersApi/index.js
+++ playersApi/index.js
@@ -3,8 +3,20 @@ const app = express()
 const playersData = require('./data/players.json')
 const port = process.env.PORT || 5050
 
+const getPlayersByTeam = (teamId) => {
+  const players = playersData.players.filter(
+    player => player.teamId == teamId
+  )
+
+  return players
+}
+
 app.get('/players', (req, res) =>{
-  res.json(playersData)
+  const { teamId } = req.query
+  if(!teamId) return res.json(playersData.players)
+
+  const playersFound = getPlayersByTeam(teamId)
+  res.json(playersFound)
 })
 
 app.listen(port, () => {
--- teamApi/index.js
+++ teamApi/index.js
@@ -3,8 +3,19 @@ const app = express()
 const teamData = require('./data/teams.json')
 const port = process.env.PORT || 4040
 
+const matchName = (nameWord, name) => {
+  const regex = new RegExp(nameWord.toLowerCase(),'g')
+  return name.toLowerCase().search(regex) != -1
+}
+
 app.get('/teams', (req, res) =>{
-  res.json(teamData)
+  const { search } = req.query
+  if(!search) return res.json(teamData)
+
+  const teamsMatched = teamData.teams.filter(
+    team => matchName(search, team.teamName)
+  )
+  res.json(teamsMatched)
 })
 
 app.listen(port, () => {
