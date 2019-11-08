4d1b7e731afa0aa4660171f9d34d59bacd618e4c
Bruno Casotto <casottoalves@hotmail.com>
2019-11-07 23:33:24 -0300

    docker compose apis mock configuration

--- playersApi/index.js
+++ playersApi/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const playersData = require('./data/players.json')
-const port = process.env.PORT || 4040
+const port = process.env.PORT || 5050
 
 app.get('/players', (req, res) =>{
   res.json(playersData)
